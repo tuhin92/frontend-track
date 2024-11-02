5514dc8438d10a87e3fb3102b717fff6c4c6bf17
tuhin92 <tuhinalmamun71m@gmail.com>
2024-11-02 20:30:09 +0600

    difference between state and reference

--- src/Components/USE_Ref.jsx
+++ src/Components/USE_Ref.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useRef } from "react";
 
+// useRef return object not array 
+// It's a reference, it's not a state. We can't chage the state value like use state 
 const USE_Ref = () => {
-    return (
-        <div>
-            <h2 className='text-4xl text-black text-center my-6'>useRef</h2>
-        </div>
-    );
+  const myRef = useRef("Bnagladesh");
+  console.log(myRef);
+
+  return (
+    <div>
+      <h2 className="text-4xl text-black text-center my-6">useRef</h2>
+    </div>
+  );
 };
 
 export default USE_Ref;
